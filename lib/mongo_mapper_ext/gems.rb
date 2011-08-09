fb3449f026c0fb06f524b068a8d5d2be74e6a225
Dana Merrick <dmerrick@brightcove.com>
2011-08-09 13:09:37 -0400

    gemmmms

--- lib/mongo_mapper_ext/gems.rb
+++ lib/mongo_mapper_ext/gems.rb
@@ -5,10 +5,10 @@ gem 'activemodel', '>= 3.0.4'
 gem 'bson', '>= 1.3.0'
 gem 'bson_ext', '>= 1.3.0'
 gem 'mongo', '>= 1.3.0'
-gem 'plucky', '0.3.6'
+gem 'plucky', '>= 0.3.6'
 
-gem 'mini_magick',   '3.2.1'
-gem 'subexec',       '0.0.4'
+gem 'mini_magick',   '>= 3.2.1'
+gem 'subexec',       '>= 0.0.4'
 
 # core gems
 gem 'mongo_mapper', '>= 0.9.0'
