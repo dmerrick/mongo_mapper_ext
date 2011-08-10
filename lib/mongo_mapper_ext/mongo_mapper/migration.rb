cf543a43d92480ffd2c03faff4ed08e5d929de32
Dana Merrick <dmerrick@brightcove.com>
2011-08-10 13:17:31 -0400

    tweaaaaks

--- lib/mongo_mapper_ext/mongo_mapper/migration.rb
+++ lib/mongo_mapper_ext/mongo_mapper/migration.rb
@@ -54,12 +54,13 @@ module MongoMapper::Migration
       definition = MigrationDefinition.new
       block.call definition
       
-      puts "exiting MigrationDefinition#define"
-      
       definitions[database_alias][version] = definition
     end
             
-    def update database_alias, version  
+    def update database_alias, version
+
+      puts "in MigrationDefinition#update"
+
       database_alias = database_alias.to_s
       db = MongoMapper.databases[database_alias]
       
@@ -76,16 +77,25 @@ module MongoMapper::Migration
     end
         
     def metadata db
+      
+      puts "in MigrationDefinition#metadata"
+
       col = db.collection 'db_metadata'
       col.find_one || {'version' => 0}
     end
     
     def definitions
+
+      puts "in MigrationDefinition#definitions"
+
       @definitions ||= Hash.new{|h, k| h[k] = []} 
     end
     
     protected
-      def increase_db_version database_alias, db    
+      def increase_db_version database_alias, db
+        
+        puts "in MigrationDefinition#increase_db_version"
+
         m = metadata(db)
         migration = definitions[database_alias][m['version'] + 1]        
         raise "No upgrade for version #{m['version'] + 1} of '#{database_alias}' Database!" unless migration and migration.up
@@ -99,6 +109,9 @@ module MongoMapper::Migration
       end
       
       def decrease_db_version database_alias, db
+        
+        puts "in MigrationDefinition#decrease_db_version"
+
         m = metadata(db)
         migration = definitions[database_alias][m['version']]        
         raise "No downgrade for version #{m['version']} of '#{database_alias}' Database!" unless migration and migration.down
@@ -113,6 +126,9 @@ module MongoMapper::Migration
 
       
       def update_metadata db, metadata
+        
+        puts "in MigrationDefinition#update_metadata"
+
         col = db.collection 'db_metadata'
         col.save metadata.to_hash
       end
